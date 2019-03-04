e18e7f9f2e46c8bcff13bd8302d1727e60a547e1
masayuki1984 <xianx.standalone@gmail.com>
2019-03-04 23:46:01 +0900

    「REIT」CSVファイル取得日次処理のリファクタリング
    - CSV取得日次処理系のスクリプトを「/script/crawler」配下にまとめるようにする
    - CSV取得日次処理系のスクリプトのログ出力先を「/var/local/crawler」配下に出力するようにする
    - ダウンロード先のディレクトリをコンフィグファイルから取得するように修正

---
+++ script/crawler/crawl_tosho_reit_stock.py
@@ -0,0 +1,82 @@
+#coding:utf-8
+
+import crawler_config
+import logging
+import requests
+import csv
+import datetime
+import sys
+import slackweb
+import configparser
+
+
+BASE_DIR = '/usr/local/script/'
+account = BASE_DIR + 'config/account.ini'
+account_config = configparser.ConfigParser()
+account_config.read(account, 'UTF-8')
+
+path = BASE_DIR + 'config/path.ini'
+path_config = configparser.ConfigParser()
+path_config.read(path, 'UTF-8')
+
+KABU_PLUS_ID = account_config.get('kabu_plus', 'KABU_PLUS_ID')
+KABU_PLUS_PW = account_config.get('kabu_plus', 'KABU_PLUS_PW')
+DOWNLOAD_SAVE_DIR = path_config.get('csv_path', 'data_base') + path_config.get('csv_path', 'tosho_reit_stock_prices')
+
+args = sys.argv
+
+class CSVFile:
+
+    KABU_PLUS_ID = ""
+    KABU_PLUS_PW = ""
+
+    def __init__(self, KABU_PLUS_ID, KABU_PLUS_PW):
+        self.KABU_PLUS_ID = KABU_PLUS_ID
+        self.KABU_PLUS_PW = KABU_PLUS_PW
+
+    def retrieve_csv_file(self, url):
+        res = requests.get(url, auth=(KABU_PLUS_ID, KABU_PLUS_PW))
+        if res.status_code != 200:
+            log.error('ステータスコードが200以外')
+            slack.notify(text="REIT取得処理：異常終了")
+            sys.exit(1)
+        data = res.content.decode('shift-jis')
+        return data
+
+    def to_csv(self, data, file=None):
+        if file:
+            with open(file, 'w', encoding='utf-8') as f:
+                writer = csv.writer(f)
+                reader = csv.reader(data.splitlines())
+                for row in reader:
+                    writer.writerow(row)
+
+
+if __name__ == '__main__':
+    # モジュール名でロガーを生成する(メインモジュールは 名前が '__main__' になる)
+    log = logging.getLogger(__name__)
+
+    # Slack Incoming webhook設定
+    slack_log_url = account_config.get('slack', 'slack_log_url')
+    slack = slackweb.Slack(url=slack_log_url)
+
+    # 対象の日付を設定（引数でYYYYMMDD形式で日付を入れるとその日付のファイルを対象とする）
+    if len(args) < 2:
+        TODAY = datetime.date.today()
+    else:
+        TARGET_DAY = args[1]
+        TODAY = datetime.datetime(int(TARGET_DAY[:4]), int(TARGET_DAY[4:6]), int(TARGET_DAY[-2:]))
+    file_name_date_part = str(TODAY.year) + '{:0=2}'.format(TODAY.month) + '{:0=2}'.format(TODAY.day)
+
+    log.info('REIT取得処理 : 開始')
+    # CSVファイルを取り扱うクラスの作成
+    csv_file = CSVFile(KABU_PLUS_ID, KABU_PLUS_PW)
+
+    file_name = 'tosho-reit-stock-prices_' + file_name_date_part + '.csv'
+    URL = 'https://csvex.com/kabu.plus/csv/tosho-reit-stock-prices/daily/' + file_name
+    log.info('CSVファイル取得処理（REIT）ファイル名:' + file_name)
+    data = csv_file.retrieve_csv_file(URL)
+    csv_file.to_csv(data, DOWNLOAD_SAVE_DIR + file_name)
+    log.info('REIT取得処理 : 終了')
+    slack.notify(text="REIT取得処理：正常終了")
+    sys.exit(0)
