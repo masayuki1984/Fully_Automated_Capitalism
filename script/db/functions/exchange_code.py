01961a9266534a7e0c9c8b38fa0ad9a148f960eb
masayuki1984 <xianx.standalone@gmail.com>
2019-03-06 00:08:57 +0900

    「japana_all_stock_prices」テーブルINSERT日次処理のリファクタリング
    - 対象CSVファイルのPATHをコンフィグファイルから取得する
    - スクリプトのディレクトリを「/usr/local/script/db」にまとめる
    - 市場コードと業績タイプを変換する関数を1ファイルにまとめて共通化を図る

---
+++ script/db/functions/exchange_code.py
@@ -0,0 +1,100 @@
+#coding:utf-8
+
+def get_stock_exchange_code(stock_exchange_name):
+    if stock_exchange_name == '東証一部':
+        return 1
+    elif stock_exchange_name == '東証二部':
+        return 2
+    elif stock_exchange_name == 'JQS':
+        return 3
+    elif stock_exchange_name == 'JQG':
+        return 4
+    elif stock_exchange_name == '東証マザ':
+        return 5
+    elif stock_exchange_name == '名証一部':
+        return 6
+    elif stock_exchange_name == '名証二部':
+        return 7
+    elif stock_exchange_name == '名証セント':
+        return 8
+    elif stock_exchange_name == '札証':
+        return 9
+    elif stock_exchange_name == '札証アンビ':
+        return 10
+    elif stock_exchange_name == '福証':
+        return 11
+    elif stock_exchange_name == '福証QB':
+        return 12
+    else:
+        return 'null'
+
+
+def get_industry_type(industry_name):
+    if industry_name == '水産・農林':
+        return 1
+    elif industry_name == '鉱業':
+        return 2
+    elif industry_name == '建設':
+        return 3
+    elif industry_name == '食料品':
+        return 4
+    elif industry_name == '繊維製品':
+        return 5
+    elif industry_name == 'パルプ・紙':
+        return 6
+    elif industry_name == '化学':
+        return 7
+    elif industry_name == '医薬品':
+        return 8
+    elif industry_name == '石油・石炭':
+        return 9
+    elif industry_name == 'ゴム製品':
+        return 10
+    elif industry_name == 'ガラス土石':
+        return 11
+    elif industry_name == '鉄鋼':
+        return 12
+    elif industry_name == '非鉄金属':
+        return 13
+    elif industry_name == '金属製品':
+        return 14
+    elif industry_name == '機械':
+        return 15
+    elif industry_name == '電気機器':
+        return 16
+    elif industry_name == '輸送用機器':
+        return 17
+    elif industry_name == '精密機器':
+        return 18
+    elif industry_name == 'その他製品':
+        return 19
+    elif industry_name == '電気・ガス':
+        return 20
+    elif industry_name == '陸運':
+        return 21
+    elif industry_name =='海運':
+        return 22
+    elif industry_name == '空運':
+        return 23
+    elif industry_name == '倉庫・運輸':
+        return 24
+    elif industry_name == '情報通信':
+        return 25
+    elif industry_name == '卸売':
+        return 26
+    elif industry_name == '小売':
+        return 27
+    elif industry_name == '銀行':
+        return 28
+    elif industry_name == '証券・先物':
+        return 29
+    elif industry_name == '保険':
+        return 30
+    elif industry_name == 'その他金融':
+        return 31
+    elif industry_name == '不動産':
+        return 32
+    elif industry_name == 'サービス':
+        return 33
+    else:
+        return 'null'
--- script/db/import_japan_all_stock_prices.py
+++ script/db/import_japan_all_stock_prices.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 
-import logging_config
+import db_config
+from functions import exchange_code
 import logging
 import datetime
 import csv
@@ -16,7 +17,7 @@ account_config = configparser.ConfigParser()
 account_config.read(account, 'UTF-8')
 
 path = BASE_DIR + 'config/path.ini'
-path_config = configparser.COnfigParser()
+path_config = configparser.ConfigParser()
 path_config.read(path, 'UTF-8')
 
 # Constants
@@ -25,115 +26,15 @@ DB_PASSWORD = account_config.get('db', 'DB_PASSWORD')
 DB_HOST = account_config.get('db', 'DB_HOST')
 DB_DATABASE = account_config.get('db', 'DB_DATABASE')
 TABLE = 'japan_all_stock_prices'
-CSV_FILE_DIR = BASE_DIR + "/../data/japan_all_stock_prices/"
+CSV_FILE_DIR = path_config.get('csv_path', 'data_base') + path_config.get('csv_path', 'japan_all_stock_prices')
 
 args = sys.argv
-
-def get_stock_exchange_code(stock_exchange_name):
-    if stock_exchange_name == '東証一部':
-        return 1
-    elif stock_exchange_name == '東証二部':
-        return 2
-    elif stock_exchange_name == 'JQS':
-        return 3
-    elif stock_exchange_name == 'JQG':
-        return 4
-    elif stock_exchange_name == '東証マザ':
-        return 5
-    elif stock_exchange_name == '名証一部':
-        return 6
-    elif stock_exchange_name == '名証二部':
-        return 7
-    elif stock_exchange_name == '名証セント':
-        return 8
-    elif stock_exchange_name == '札証':
-        return 9
-    elif stock_exchange_name == '札証アンビ':
-        return 10
-    elif stock_exchange_name == '福証':
-        return 11
-    elif stock_exchange_name == '福証QB':
-        return 12
-    else:
-        return 'null'
-
-
-def get_industry_type(industry_name):
-    if industry_name == '水産・農林':
-        return 1
-    elif industry_name == '鉱業':
-        return 2
-    elif industry_name == '建設':
-        return 3
-    elif industry_name == '食料品':
-        return 4
-    elif industry_name == '繊維製品':
-        return 5
-    elif industry_name == 'パルプ・紙':
-        return 6
-    elif industry_name == '化学':
-        return 7
-    elif industry_name == '医薬品':
-        return 8
-    elif industry_name == '石油・石炭':
-        return 9
-    elif industry_name == 'ゴム製品':
-        return 10
-    elif industry_name == 'ガラス土石':
-        return 11
-    elif industry_name == '鉄鋼':
-        return 12
-    elif industry_name == '非鉄金属':
-        return 13
-    elif industry_name == '金属製品':
-        return 14
-    elif industry_name == '機械':
-        return 15
-    elif industry_name == '電気機器':
-        return 16
-    elif industry_name == '輸送用機器':
-        return 17
-    elif industry_name == '精密機器':
-        return 18
-    elif industry_name == 'その他製品':
-        return 19
-    elif industry_name == '電気・ガス':
-        return 20
-    elif industry_name == '陸運':
-        return 21
-    elif industry_name =='海運':
-        return 22
-    elif industry_name == '空運':
-        return 23
-    elif industry_name == '倉庫・運輸':
-        return 24
-    elif industry_name == '情報通信':
-        return 25
-    elif industry_name == '卸売':
-        return 26
-    elif industry_name == '小売':
-        return 27
-    elif industry_name == '銀行':
-        return 28
-    elif industry_name == '証券・先物':
-        return 29
-    elif industry_name == '保険':
-        return 30
-    elif industry_name == 'その他金融':
-        return 31
-    elif industry_name == '不動産':
-        return 32
-    elif industry_name == 'サービス':
-        return 33
-    else:
-        return 'null'
-
 
 if __name__ == '__main__':
     # モジュール名でロガーを生成する(メインモジュールは 名前が '__main__' になる)
     log = logging.getLogger(__name__)
     # Slack Incoming webhook設定
-    slack_log_url = config.get('slack', 'slack_log_url')
+    slack_log_url = account_config.get('slack', 'slack_log_url')
     slack = slackweb.Slack(url=slack_log_url)
 
     log.info('日本株全銘柄テーブルインポート処理 : 開始')
@@ -166,8 +67,8 @@ if __name__ == '__main__':
                 security_code = row[0] if row[0] != '-' else 'null'
                 dt = file_name_date_part
                 company_name = row[1] if row[1] != '-' else 'null'
-                stock_exchange_code = get_stock_exchange_code(row[2])
-                industry_type = get_industry_type(row[3])
+                stock_exchange_code = exchange_code.get_stock_exchange_code(row[2])
+                industry_type = exchange_code.get_industry_type(row[3])
                 opening_price = row[9] if row[9] != '-' else 'null'
                 closing_price = row[5] if row[5] != '-' else 'null'
                 high_price = row[10] if row[10] != '-' else 'null'
