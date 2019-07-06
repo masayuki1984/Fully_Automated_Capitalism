34e26c9b8ed236d1a2b915bfb9bb0a5fa6c3cc5a
masayuki1984 <masayuki_kato1984@icloud.com>
2019-07-06 15:45:45 +0900

    ConoHa告知情報取得処理のread_status既読化

--- script/conoha/functions/api_common.py
+++ script/conoha/functions/api_common.py
@@ -3,9 +3,12 @@
 import requests
 import json
 
+# Constants
+SLASH = "/"
 
-def get_token(BASE_ENDPOINT, TOKEN_PATH, TENANT_ID, USER_NAME, PASSWARD, ACCEPT_JSON):
-    url = BASE_ENDPOINT + TOKEN_PATH
+
+def get_token(BASE_ENDPOINT, SUB_PATH, TENANT_ID, USER_NAME, PASSWARD, ACCEPT_JSON):
+    url = BASE_ENDPOINT + SUB_PATH
     body = {
         "auth":{
             "passwordCredentials":{
@@ -24,7 +27,38 @@ def get_token(BASE_ENDPOINT, TOKEN_PATH, TENANT_ID, USER_NAME, PASSWARD, ACCEPT_
 
     # HTTPステータスコードが「200:OK」
     if respons.status_code == requests.codes.ok:
-        return respons
+        respons_json = json.loads(respons.text)
+        return respons_json['access']['token']['id']
     else:
         return respons.raise_for_status()
 
+def get_notification_code_list(BASE_ENDPOINT, SUB_PATH, TENANT_ID, ACCEPT_JSON, token):
+    url = BASE_ENDPOINT + SLASH + TENANT_ID + SUB_PATH
+    respons = requests.get(
+        url,
+        params={'limit': 20},
+        headers={'Accept': ACCEPT_JSON, 'X-Auth-Token': token}
+    )
+    if respons.status_code == requests.codes.ok:
+        respons_json = json.loads(respons.text)
+        return respons_json['notifications']
+    else:
+        return respons.raise_for_status()
+
+def set_read_status(BASE_ENDPOINT, SUB_PATH, TENANT_ID, ACCEPT_JSON, token, notice_code, read_status):
+    url = BASE_ENDPOINT + SLASH + TENANT_ID + SUB_PATH + SLASH + str(notice_code)
+    body = {
+        "notification":{
+            "read_status": read_status
+        }
+    }
+    response = requests.put(
+        url,
+        json.dumps(body),
+        headers={'Accept': ACCEPT_JSON, 'X-Auth-Token': token}
+    )
+    if response.status_code == requests.codes.ok:
+        response_json = json.loads(response.text)
+        return response_json['notification']['read_status']
+    else:
+        return response.raise_for_status()
--- script/conoha/get_notice.py
+++ script/conoha/get_notice.py
@@ -23,8 +23,10 @@ TENANT_ID = conoha_config.get('common', 'TENANT_ID')
 USER_NAME = conoha_config.get('common', 'USER_NAME')
 PASSWARD = conoha_config.get('common', 'PASSWARD')
 ACCEPT_JSON = conoha_config.get('header', 'ACCEPT_JSON')
-BASE_ENDPOINT = conoha_config.get('base_endpoint', 'ENDPOINT_IDENTITY_SERVICE')
+IDENTITY_ENDPOINT = conoha_config.get('base_endpoint', 'ENDPOINT_IDENTITY_SERVICE')
+ACCOUNT_ENDPOINT = conoha_config.get('base_endpoint', 'ENDPOINT_ACCOUNT_SERVICE')
 TOKEN_PATH = conoha_config.get('endpoint', 'TOKENS')
+NOTIFICATIONS = conoha_config.get('endpoint', 'NOTIFICATIONS')
 
 
 if __name__ == "__main__":
@@ -38,20 +40,45 @@ if __name__ == "__main__":
 
     # APIトークン取得
     try:
-        token = api_common.get_token(BASE_ENDPOINT, TOKEN_PATH, TENANT_ID, USER_NAME, PASSWARD, ACCEPT_JSON)
+        token = api_common.get_token(IDENTITY_ENDPOINT, TOKEN_PATH, TENANT_ID, USER_NAME, PASSWARD, ACCEPT_JSON)
     except requests.RequestException as e:
-        log.error(e)
+        log.error("APIトークン取得 失敗 : " + str(e))
+        slack.notify(text="【ERROR】ConoHa 告知情報取得処理失敗しました。ログを確認してください。")
         sys.exit(1)
-    print(token.text)
 
     # 告知一覧取得処理
+    try:
+        notice_list = api_common.get_notification_code_list(ACCOUNT_ENDPOINT, NOTIFICATIONS, TENANT_ID, ACCEPT_JSON, token)
+    except requests.RequestException as e:
+        log.error("告知一覧取得処理 失敗 : " + str(e))
+        slack.notify(text="【ERROR】ConoHa 告知情報取得処理失敗しました。ログを確認してください。")
+        sys.exit(1)
 
+    title_list = list()
+    # 各告知をslack通知
+    for notice in notice_list:
+        if notice['read_status'] == 'Read':
+            continue
+        title_list.append(notice['title'])
+        # 告知ステータスを既読に変更
+        try:
+            notice_code = notice['notification_code']
+            read_status = 'Read'
+            chenge_status = api_common.set_read_status(
+                ACCOUNT_ENDPOINT,
+                NOTIFICATIONS,
+                TENANT_ID,
+                ACCEPT_JSON,
+                token,
+                notice_code,
+                read_status
+            )
+            if chenge_status != read_status:
+                log.warn("既読ステータス変更失敗 : " + notice['title'])
+        except requests.RequestException as e:
+            log.error("告知ステータス変更 失敗 : " + str(e))
+            slack.notify(text="【ERROR】ConoHa 告知情報取得処理失敗しました。ログを確認してください。")
+            sys.exit(1)
 
-    # 告知詳細取得(アイテム指定)
-
-
-    # 告知ステータスを既読に変更
-
-
-
-
+    slack.notify(text='\n'.join(title_list))
+    log.info('ConoHa告知一覧取得 : 終了')
